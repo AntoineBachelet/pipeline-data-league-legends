c404ee28f828a04972f7d689f0fe834fda7dd826
Antoine Bachelet <a.bachelet708@gmail.com>
2026-04-07 15:41:47 +0200

    feat: add schema player soloqueue account

---
+++ dagster_lol/assets/silver/schemas/player_soloqueue_accounts.py
@@ -0,0 +1,13 @@
+"""Schema de données pour les comptes soloqueue des joueurs leaguepedia."""
+
+
+class PlayerSoloqueueAccountsSchema:
+    """Schéma avec métadonnées de format pour les comptes soloqueue."""
+
+    PLAYER_SOLOQUEUE_ACCOUNTS_SCHEMA = {
+        "player_overview_page": {"type": "str", "missing_value": None},
+        "region": {"type": "str", "missing_value": None},
+        "game_name": {"type": "str", "missing_value": None},
+        "tag_line": {"type": "str", "missing_value": None},
+        "full_id": {"type": "str", "missing_value": None},
+    }
