9bbbec48d58ca8dfb1998715b149091c311af944
Antoine Bachelet <a.bachelet708@gmail.com>
2026-04-08 16:37:07 +0200

    feat: add bronze dataretrieving of lol pros entire ladder

--- dagster_lol/assets/bronze/__init__.py
+++ dagster_lol/assets/bronze/__init__.py
@@ -1,4 +1,5 @@
 from .health_check import ping
 from .leaguepedia import tournaments_bronze, tournament_rosters_bronze, players_bronze
+from .lolpros import lolpros_ladder_bronze
 
-__all__ = ["ping", "tournaments_bronze", "tournament_rosters_bronze", "players_bronze"]
+__all__ = ["ping", "tournaments_bronze", "tournament_rosters_bronze", "players_bronze", "lolpros_ladder_bronze"]
